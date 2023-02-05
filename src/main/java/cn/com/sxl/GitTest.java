e3864581bc41a60a2a5f81694c2c1f5503a6eebb
sxl <sunxiaolong9999@126.com>
2023-02-05 13:33:06 +0800

    hot-fix2 commit2

--- src/main/java/cn/com/sxl/GitTest.java
+++ src/main/java/cn/com/sxl/GitTest.java
@@ -4,6 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello word");
         System.out.println("hello word2");
+        System.out.println("hello word3");
         System.out.println("hello word4");
     }
 }
